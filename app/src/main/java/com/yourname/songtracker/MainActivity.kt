c5f85f84f7eadb53848e5933da65fa52fc02254f
Rishflash21 <154812104+Rishflash21@users.noreply.github.com>
2025-07-20 19:18:46 +0530

    Update MainActivity.kt

--- app/src/main/java/com/yourname/songtracker/MainActivity.kt
+++ app/src/main/java/com/yourname/songtracker/MainActivity.kt
@@ -1,1 +1,26 @@
+package com.yourname.songtracker
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.yourname.songtracker.ui.theme.SongTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val database = SongDatabase.getDatabase(applicationContext)
+        val dao = database.songDao()
+
+        setContent {
+            SongTrackerTheme {
+                val viewModel: SongViewModel = viewModel(
+                    factory = SongViewModelFactory(dao)
+                )
+
+                SongTrackerUI(viewModel)
+            }
+        }
+    }
+}

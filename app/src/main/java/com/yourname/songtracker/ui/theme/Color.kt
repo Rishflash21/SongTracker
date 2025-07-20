8e699a643cb2c64ba21300e7c396269ee75a5520
Rishflash21 <154812104+Rishflash21@users.noreply.github.com>
2025-07-20 19:21:45 +0530

    Create Color.kt

---
+++ app/src/main/java/com/yourname/songtracker/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.yourname.songtracker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF00C4FF)
+val Background = Color(0xFF121212)
+val Surface = Color(0xFF1E1E1E)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFFB0B0B0)

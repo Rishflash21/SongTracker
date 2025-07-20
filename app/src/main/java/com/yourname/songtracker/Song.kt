f3236630a1d77ed624d2fd680e5764b7da805583
Rishflash21 <154812104+Rishflash21@users.noreply.github.com>
2025-07-20 19:50:44 +0530

    Update Song.kt

--- app/src/main/java/com/yourname/songtracker/Song.kt
+++ app/src/main/java/com/yourname/songtracker/Song.kt
@@ -3,7 +3,7 @@ package com.yourname.songtracker
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "songs")
+@Entity
 data class Song(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String
